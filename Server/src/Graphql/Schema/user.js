5a60543619747db44f21e279ffa5934f3c0104e4
Deepakkushwaha01 <deepakkushwaha95820@gmail.com>
2024-03-10 14:09:29 +0530

    make type folder

--- Server/src/Graphql/Schema/user.js
+++ Server/src/Graphql/Schema/user.js
@@ -1,36 +1,10 @@
-import {
-  GraphQLID,
-  GraphQLInputObjectType,
-  GraphQLNonNull,
-  GraphQLObjectType,
-  GraphQLString,
-} from "graphql";
-
-export const UserType = new GraphQLObjectType({
-  name: "User",
-  fields: {
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    email: { type: GraphQLString },
-  },
-});
-
-export const UserInputType = new GraphQLInputObjectType({
-  name: "UserInput",
-  fields: {
-    name: { type: new GraphQLNonNull(GraphQLString) },
-    email: { type: new GraphQLNonNull(GraphQLString) },
-    phone: { type: new GraphQLNonNull(GraphQLString) },
-    password: { type: new GraphQLNonNull(GraphQLString) },
-    Cpassword: { type: new GraphQLNonNull(GraphQLString) },
-    account_type: { type: new GraphQLNonNull(GraphQLString) },
-  },
-});
+import { GraphQLNonNull } from "graphql";
+import { UserInputType, UserOutputType } from "../types/user/UserInputType.js";
 
 export default {
-  type: UserType,
+  type: UserOutputType,
   args: { type: new GraphQLNonNull(UserInputType) },
-  resolve: async (_, args,context) => {
+  resolve: async (_, args, context) => {
     return {
       id: 2,
       name: "dee",
